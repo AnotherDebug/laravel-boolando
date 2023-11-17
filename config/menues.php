de965f8f6af4f6f9cd2b40793e240ce8d565814a
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-17 16:20:06 +0100

    [ADD]: array of arrays menues php

---
+++ config/menues.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    "topbarMenu" => [
+        [
+            "name" => "woman",
+            "text" => "Donna"
+        ],
+        [
+            "name" => "man",
+            "text" => "Uomo"
+        ],
+        [
+            "name" => "children",
+            "text" => "Bambino"
+        ]
+    ],
+    "topbarIcons" => [
+        [
+            "icon" => "fa-regular fa-user",
+            "text" => "user"
+        ],
+        [
+            "icon" => "fa-regular fa-heart",
+            "text" => "heart"
+        ],
+        [
+            "icon" => "fa-solid fa-bag-shopping",
+            "text" => "bag shopping"
+        ]
+    ],
+    "footerMenu" => [
+        [
+            "name" => "",
+            "text" => "Informazioni legali"
+        ],
+        [
+            "name" => "",
+            "text" => "Informativa sulla privacy"
+        ],
+        [
+            "name" => "",
+            "text" => "Diritto di recesso"
+        ]
+    ],
+    "footerIcons" => [
+        [
+            "icon" => "fa-brands fa-square-twitter",
+            "text" => "twitter"
+        ],
+        [
+            "icon" => "fa-brands fa-square-facebook",
+            "text" => "facebook"
+        ],
+        [
+            "icon" => "fa-brands fa-square-instagram",
+            "text" => "instagram"
+        ],
+        [
+            "icon" => "fa-brands fa-square-pinterest",
+            "text" => "pinterest"
+        ],
+        [
+            "icon" => "fa-brands fa-youtube",
+            "text" => "youtube"
+        ]
+    ]
+];
