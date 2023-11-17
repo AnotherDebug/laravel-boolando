accfe925f0b242d5ba94df48833e4167ce09807a
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-17 16:46:25 +0100

    [ADD]: woman php

---
+++ resources/views/woman.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Pagina Donna</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat velit eveniet, accusamus, quis deleniti rerum cumque debitis similique minus reiciendis ea? Dolorem nesciunt soluta minima molestias repellat minus quae asperiores id sequi fuga distinctio fugiat accusantium, eaque qui sapiente impedit quam quidem numquam quas saepe corrupti cumque sed assumenda. Beatae!</p>
+
+@endsection
