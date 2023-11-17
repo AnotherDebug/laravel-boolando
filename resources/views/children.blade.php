db1505868dd78348559ba582ea398d44fcefea2f
AnotherDebug <antonio.faddanno@gmail.com>
2023-11-17 16:50:14 +0100

    [ADD]: children php

---
+++ resources/views/children.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Pagina Bambino</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat velit eveniet, accusamus, quis deleniti rerum cumque debitis similique minus reiciendis ea? Dolorem nesciunt soluta minima molestias repellat minus quae asperiores id sequi fuga distinctio fugiat accusantium, eaque qui sapiente impedit quam quidem numquam quas saepe corrupti cumque sed assumenda. Beatae!</p>
+
+@endsection
+
+
+@section('title')
+
+| Moda Bambino
+
+@endsection
